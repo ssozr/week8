1b6822022e944f77a39e8980cb8ea4ca35011dad
ssozr <a917382460@gmail.com>
2022-09-10 20:26:53 +0800

    Update 2022-09-10T12:26:37.655Z

---
+++ assets/js/all.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+  $('.seach_btn').on('click', function () {
+    $('.seach_input').toggle();
+  });
+});
+//# sourceMappingURL=all.js.map
