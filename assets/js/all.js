2e864701f018bda35f5ae4ec06619cb4ae92e868
ssozr <a917382460@gmail.com>
2022-09-12 21:45:19 +0800

    Update 2022-09-12T13:45:15.725Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,4 +6,37 @@ $(function () {
     $('.seach_input').toggle();
   });
 });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 48,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+});
 //# sourceMappingURL=all.js.map
